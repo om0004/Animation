774aab660b6d6932c3aca30caf9e1847cf5de773
om <60003596+om0004@users.noreply.github.com>
2021-04-21 22:08:22 +0530

    Add files via upload

---
+++ AnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationApp.swift
+//  Animation
+//
+//  Created by om on 10/04/21.
+//
+
+import SwiftUI
+
+@main
+struct AnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
